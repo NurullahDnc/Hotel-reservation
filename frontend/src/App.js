c62ec0f328a510de4da8e927fae522740faa3b25
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2024-02-19 15:28:57 +0300

    Redux, Router and Layout structure was established.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,51 @@
+import MainLayout from "./layout/MainLayout";
+import React from 'react'
+import { Routes, Route, BrowserRouter as Router, } from 'react-router-dom';
+import Home from "./pages/Home";
+import Navbar from "./comporents/navbar/Navbar";
+import AdminLayout from "./layout/AdminLayout";
 
 
 function App() {
+
   return (
     <div className="App">
-      merhaba
-      
-          </div>
+
+      <Router>
+        <Routes>
+          <Route
+            path="/*"
+            element={
+              <MainLayout>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+
+
+                  <Route path="*" element={<div>Sayfa Bulunamadı</div>} />
+                </Routes>
+              </MainLayout>
+            }
+          />
+  
+
+          <Route
+            path="/admin/*"
+            element={
+              <AdminLayout > 
+                <Routes>
+
+                <Route path="*" element={<div>Sayfa Bulunamadı</div>} />
+
+                </Routes>
+              </AdminLayout>
+            }
+          />
+
+
+        </Routes>
+      </Router>
+
+    </div>
   );
 }
 
---
+++ frontend/src/layout/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../comporents/navbar/Navbar'
+import Footer from '../comporents/footer/Footer'
+
+
+const MainLayout = ({children}) => {
+  return (
+    <>
+        <Navbar />
+        {children}
+        <Footer />
+        
+    </>
+  )
+}
+
+export default MainLayout
