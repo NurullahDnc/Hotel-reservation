73d8836c2dc5fcf78a3df7178d55da1c544fa4c5
Nazlı Nur ESMERAY <96131988+nazlinres01@users.noreply.github.com>
2024-02-24 16:45:22 +0300

    footer added

--- frontend/src/comporents/footer/Footer.jsx
+++ frontend/src/comporents/footer/Footer.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { Container, Grid, Typography, Link, TextField, Button, Divider } from '@mui/material';
 
 const Footer = () => {
   return (
-    <div>
-        footer
-    </div>
-  )
+    <footer className='footer'>
+      <Container className='footer-container'>
+        <Grid container spacing={3} className='footer-container-grid'>
+
+          <Grid item xs={12} md={2} className='footer-container-grid-logo'>
+            {/*<img src={logoImage} alt="Logo" style={{ width: '100%', height: 'auto' }} />*/}
+            LOGO
+            <Typography variant="body2" className='logo-description'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non sem in urna porttitor tincidunt.
+            </Typography>
+          </Grid>
+
+          <Divider orientation="vertical" flexItem className='divider' />
+
+          <Grid item xs={12} md={2.8} className='footer-container-grid-menu'>
+            <Typography variant="h6" >Menu </Typography>
+            <Link href="#">Hakkımızda</Link>
+            <Link href="#">İletişim</Link>
+            <Link href="#">Şartlar ve Koşullar</Link>
+            <Link href="#">Gizlilik Politikası</Link>
+          </Grid>
+
+          <Divider orientation="vertical" flexItem className='divider' />
+
+          <Grid item xs={12} md={2.8} className='footer-container-grid-follow'>
+            <Typography variant="h6">Bizi Takip Edin:</Typography>
+            <Link href="#">Facebook</Link>
+            <Link href="#">Twitter</Link>
+            <Link href="#">Instagram</Link>
+          </Grid>
+
+          <Divider orientation="vertical" flexItem className='divider' />
+
+          <Grid item xs={12} md={2.8} className='footer-container-grid-newsletter'>
+            <Typography variant="h6">Abonelik ve Haber Bülteni</Typography>
+            {/* Abonelik ve haber bülteni formu veya input bileşeni buraya gelebilir */}
+            <TextField className='textfield' label="E-posta Adresiniz" variant="outlined" fullWidth />
+            <Button variant="contained" color="primary">Abone Ol</Button>
+          </Grid>
+        </Grid>
+
+        <Typography variant="body2" color="textSecondary" align="center" >
+          &copy; [Şirket Adı] [Yıl] - Tüm Hakları Saklıdır.
+        </Typography>
+      </Container>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
