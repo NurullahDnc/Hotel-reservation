a5647750286eb854c03c4d2ea17dddb8cbadafa6
Nazlı Nur ESMERAY <96131988+nazlinres01@users.noreply.github.com>
2024-03-08 16:18:45 +0300

    room and roomDetail added

--- frontend/src/comporents/about/About.jsx
+++ frontend/src/comporents/about/About.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import PageTitleImage from '../general/PageTitleImage';
-import { Grid, Typography, Paper } from '@mui/material';
+import {Grid} from '@mui/material';
 import PhoneIcon from '@mui/icons-material/Phone';
-import { blue } from '@mui/material/colors';
 
 const About = () => {
     const { t } = useTranslation();
@@ -12,7 +11,7 @@ const About = () => {
         <div className='about'>
             <PageTitleImage
                 image="https://img.freepik.com/free-photo/desert-sand-dunes-panoramic-view_587448-8157.jpg?t=st=1709244386~exp=1709247986~hmac=350994ef7dcf770cf9dd7968817081b721ac48da1d3dd974897dd8dfb0ed33d4&w=1380"
-                title={t('Hakkımızda')}
+                title={t('aboutUs')}
             />
 
             <div className='about-grid'>
---
+++ frontend/src/pages/RoomDetailPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RoomDetail from '../comporents/room/RoomDetail'; 
+
+const RoomDetailPage = () => {
+  return (
+    <div>
+      <RoomDetail />
+    </div>
+  );
+};
+
+export default RoomDetailPage;
